ccfb9abeaa9b079b949ec4e1bb9254ee97e1993c
Cosmin Avram <cosminavram26@gmail.com>
2020-05-18 15:47:59 +0300

    add multiple things

--- test_gcd.py
+++ test_gcd.py
@@ -5,5 +5,5 @@ def test_gcd_valid():
     assert computeGCD(60, 48) == 12
 
 def test_gcd_invalid():
-    assert computeGCD(60, 48) == 13
+    assert computeGCD(60, 48) == 13, "Test failed!"
 
--- test_user_invalid_name_fixtures.py
+++ test_user_invalid_name_fixtures.py
@@ -1,45 +1,46 @@
 from faker import Faker
 
-fake = Faker()
-
 class User:
     """
     Class for representing a user in our application
     """
+
     def __init__(self, username: str, email: str, password: str):
         self.__username = username
         self.__email = email
         self.__password = password
-    
+
     ### @property -> getter
     @property
     def username(self) -> str:
         return self.__username
-    
+
     @property
     def email(self) -> str:
         return self.__email
-    
+
     @property
     def password(self) -> str:
         return self.__password
-    
+
     ### @attribute -> setter
     @username.setter
     def username(self, val: str):
         self.__username = val
-    
+
     @password.setter
     def password(self, val: str):
         self.__password = val
-    
+
     @email.setter
     def email(self, val: str):
         self.__email = val
-    
+
     def __str__(self):
         return f"Username: {self.__username} - Password: {self.__password} - Email: {self.email}\n"
 
+
+fake = Faker()
 
 import pytest
 import ipytest # in order to use pytest in jupyter notebook
